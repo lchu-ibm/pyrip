2f1a2e3c2cbe18f4fb932302838f12b866523afd
lchu <lchu@us.ibm.com>
2019-09-11 09:40:32 -0700

    change to public github in setup

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ setup(
     version=VERSION,
     description='Raster Image Processor - a thin wrap that provides some convenient functions over gdal',
     packages=find_packages(),
-    url='https://github.ibm.com/Common-Spatiotemporal-Analytics-Library/pyrip',
+    url='https://github.com/lchu-ibm/pyrip',
     author='Linsong Chu',
     author_email='lchu@us.ibm.com',
     install_requires=['rasterio', 'pandas', 'six', 'numpy']
