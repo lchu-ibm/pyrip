a7d8d028323f0a3ce0c54d6f4b58f425786d3bd8
lchu <lchu@us.ibm.com>
2019-10-18 15:38:04 -0400

    improve cloud query function

--- pyrip/cloud.py
+++ pyrip/cloud.py
@@ -1,6 +1,7 @@
 import os
 import six
 import tempfile
+from pyrip.util import expand_bbox
 
 
 def download(cos_client, bucket, key, download_dir):
@@ -70,52 +71,107 @@ def query_tiles_with_spark(spark, api_key, instance_crn, endpoint_url, bucket, i
         return tiles
 
 
-def query_tiles(sql_client, cos_client, cos_url, target_cos_url, layers, start_date, end_date, bbox, download_dir, merge_tiles=True, force_bbox=True):
+def query_tiles(sql_client, cos_client, cos_url, target_cos_url, layers, start_date, end_date, bbox, download_dir,
+                search_buffer_in_degrees=2, selected_dates=None, merge_tiles=True, force_bbox=True, merge_by_date=True):
+
     if isinstance(layers, six.string_types):
         layers = [layers]
+
+    if isinstance(selected_dates, six.string_types) or isinstance(selected_dates, six.integer_types):
+        selected_dates = [selected_dates]
 
     # By default, the result will be written into target_cos in CSV format. We disable this and make it write as
     # parquet to improvement performance
     if sql_client.target_cos is not None:
         sql_client.target_cos = None
 
+    expanded_bbox = expand_bbox(bbox, search_buffer_in_degrees)
+
+    if selected_dates is None:
+        date_query_part = 'date between {} AND {}'.format(start_date, end_date)
+    else:
+        date_query_part = 'date in {}'.format(tuple(selected_dates))
+
+    if expanded_bbox[2] >= expanded_bbox[0]:
+        lon_query_part = 'lon between {} AND {}'.format(expanded_bbox[0], expanded_bbox[2])
+    else:
+        lon_query_part = '(lon between {} AND 180 OR lon between -180 AND {})'.format(expanded_bbox[0], expanded_bbox[2])
+
     query_str = """
     SELECT *
     FROM {0} STORED AS PARQUET
-    WHERE layer in {1} AND date between {2} AND {3}
-    AND lat between {4[1]} AND {4[3]} AND lon between {4[0]} AND {4[2]}
+    WHERE layer in {1}
+    AND {2}
+    AND lat between {3[1]} AND {3[3]}
+    AND {4}
     INTO {5} STORED AS parquet
-    """.format(cos_url, tuple(layers), start_date, end_date, bbox, target_cos_url)
+    """.format(cos_url, tuple(layers), date_query_part, expanded_bbox, lon_query_part, target_cos_url)
     # ST_Contains(ST_WKTToSQL('BOUNDINGBOX({} {}, {} {})'), ST_Point(lon, lat))
 
     df = sql_client.run_sql(query_str)
 
     if not os.path.exists(download_dir):
         os.makedirs(download_dir)
+
     if merge_tiles:
         from pyrip.image import merge
         merged_images = []
         for layer in layers:
             tmp_df = df[df['layer'] == layer].sort_values(['date', 'lat', 'lon'])
-            tiles = []
-            with tempfile.TemporaryDirectory() as tmpdir:
-                for url in tmp_df['url']:
-                    bucket, key = url.replace('cos://', '').split('/', 1)
-                    tile = download(cos_client, bucket, key, tmpdir)
-                    tiles.append(tile)
-                merged_image = os.path.join(download_dir, layer, 'merged.tif')
-                if force_bbox:
-                    merge(tiles, merged_image, bbox)
-                else:
-                    merge(tiles, merged_image)
-                merged_images.append(merged_image)
+            if merge_by_date:
+                dates = tmp_df['date'].unique()
+                for date in dates:
+                    tiles = []
+                    with tempfile.TemporaryDirectory() as tmpdir:
+                        for url in tmp_df[tmp_df['date'] == date]['url']:
+                            bucket, key = url.replace('cos://', '').split('/', 1)
+                            tile = download(cos_client, bucket, key, tmpdir)
+                            tiles.append(tile)
+                        if bbox[2] > bbox[0]:
+                            merged_image = os.path.join(download_dir, layer, str(date), 'merged.tif')
+                            if force_bbox:
+                                merge(tiles, merged_image, bbox)
+                            else:
+                                merge(tiles, merged_image)
+                            merged_images.append(merged_image)
+                        # If the given bbox spans over anti-meridian, we have to merge into two parts. Note that this
+                        # will also overwrite the value of force_bbox and set it to true.
+                        else:
+                            merged_image_1 = os.path.join(download_dir, layer, str(date), 'merged_1.tif')
+                            merged_image_2 = os.path.join(download_dir, layer, str(date), 'merged_2.tif')
+                            merge(tiles, merged_image_1, (bbox[0], bbox[1], 180, bbox[3]))
+                            merge(tiles, merged_image_2, (-180, bbox[1], bbox[2], bbox[3]))
+                            merged_images.extend([merged_image_1, merged_image_2])
+            else:
+                tiles = []
+                with tempfile.TemporaryDirectory() as tmpdir:
+                    for url in tmp_df['url']:
+                        bucket, key = url.replace('cos://', '').split('/', 1)
+                        tile = download(cos_client, bucket, key, tmpdir)
+                        tiles.append(tile)
+                    if bbox[2] > bbox[0]:
+                        merged_image = os.path.join(download_dir, layer, 'merged.tif')
+                        if force_bbox:
+                            merge(tiles, merged_image, bbox)
+                        else:
+                            merge(tiles, merged_image)
+                        merged_images.append(merged_image)
+                    # If the given bbox spans over anti-meridian, we have to merge into two parts. Note that this will
+                    # also overwrite the value of force_bbox and set it to true.
+                    else:
+                        merged_image_1 = os.path.join(download_dir, layer, 'merged_1.tif')
+                        merged_image_2 = os.path.join(download_dir, layer, 'merged_2.tif')
+                        merge(tiles, merged_image_1, (bbox[0], bbox[1], 180, bbox[3]))
+                        merge(tiles, merged_image_2, (-180, bbox[1], bbox[2], bbox[3]))
+                        merged_images.extend([merged_image_1, merged_image_2])
         return merged_images
     else:
         tiles = []
-        for layer in layers:
-            tmp_df = df[df['layer'] == layer].sort_values(['date', 'lat', 'lon'])
-            for url in tmp_df['url']:
-                bucket, key = url.replace('cos://', '').split('/', 1)
-                tile = download(cos_client, bucket, key, os.path.join(download_dir, layer))
-                tiles.append(tile)
+        for _, row in df.iterrows():
+            layer = row['layer']
+            date = row['date']
+            url = row['url']
+            bucket, key = url.replace('cos://', '').split('/', 1)
+            tile = download(cos_client, bucket, key, os.path.join(download_dir, layer, str(date)))
+            tiles.append(tile)
         return tiles
--- pyrip/util.py
+++ pyrip/util.py
@@ -13,3 +13,36 @@ def create_xyz_vrt(infile):
     ET.SubElement(layer, 'GeometryField', encoding='PointFromColumns', x='lon', y='lat')
     tree = ET.ElementTree(data_source)
     return tree
+
+
+def add_latitude(latitude, delta):
+    return max(-90, min(90, latitude + delta))
+
+
+def add_longitude(longitude, delta):
+    result = longitude + delta
+    while result > 180:
+        result -= 360
+    while result < -180:
+        result += 360
+    return result
+
+
+def diff_longitude(start_longitude, end_longitude):
+    diff = end_longitude - start_longitude
+    if diff < 0:
+        diff += 360
+    return diff
+
+
+def expand_bbox(bbox, delta):
+    if diff_longitude(bbox[0], bbox[2]) + 2 * delta > 360:
+        return -180,\
+               add_latitude(bbox[1], -delta),\
+               180,\
+               add_latitude(bbox[3], delta)
+    else:
+        return add_longitude(bbox[0], -delta),\
+               add_latitude(bbox[1], -delta),\
+               add_longitude(bbox[2], delta),\
+               add_latitude(bbox[3], delta)
